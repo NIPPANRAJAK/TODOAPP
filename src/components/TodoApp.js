3654afaf4eb4ad5207c7d6caf626d9738cc7a3db
Nippan <nippanrajak22@gmail.com>
2022-03-14 09:29:53 +0530

    second commit

---
+++ src/components/TodoApp.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from 'react'
+import "../App.css"
+
+
+// to get the data from LS
+
+const getLocalItmes = () => {
+    let list = localStorage.getItem('lists');
+    console.log(list);
+
+    if (list) {
+        return JSON.parse(localStorage.getItem('lists'));
+    } else {
+        return [];
+    }
+}
+
+const TodoApp = () => {
+
+    const [inputData, setInputData] = useState('');
+    const [items, setItems] = useState(getLocalItmes());
+    const [toggleSubmit, setToggleSubmit] = useState(true);
+    const [isEditItem, setIsEditItem] = useState(null);
+
+    const addItem = () => {
+        if (!inputData) {
+            alert('please fill data');
+        } else if (inputData && !toggleSubmit) {
+            setItems(
+                items.map((elem) => {
+                    if (elem.id === isEditItem) {
+                        return { ...elem, name: inputData }
+                    }
+                    return elem;
+                })
+            )
+            setToggleSubmit(true);
+
+            setInputData('');
+
+            setIsEditItem(null);
+        } else {
+            const allInputData = { id: new Date().getTime().toString(), name: inputData }
+            setItems([...items, allInputData]);
+            setInputData('')
+        }
+    }
+
+
+    // delete the items
+    const deleteItem = (index) => {
+        const updateditems = items.filter((elem) => {
+            return index !== elem.id;
+        });
+
+        setItems(updateditems);
+    }
+
+    // edit the item
+    //     When user click on edit button 
+
+    // 1: get the id and name of the data which user clicked to edit
+    // 2: set the toggle mode to change the submit button into edit button
+    // 3: Now update the value of the setInput with the new updated value to edit. 
+    // 4: To pass the current element Id to new state variable for reference 
+
+
+    const editItem = (id) => {
+        let newEditItem = items.find((elem) => {
+            return elem.id === id
+        });
+        // console.log(newEditItem);
+
+        setToggleSubmit(false);
+
+        setInputData(newEditItem.name);
+
+        setIsEditItem(id);
+
+    }
+
+
+    // remove all 
+    const removeAll = () => {
+        setItems([]);
+    }
+
+    // add data to localStorage
+    useEffect(() => {
+        localStorage.setItem('lists', JSON.stringify(items))
+    }, [items]);
+
+    return (
+        <>
+            <div className="main-div">
+            <h1 className='Heading'>TodoApp</h1>
+                <div className="child-div">
+                    <figure>
+                        {/* <img src={todo} alt="todologo" /> */}
+                        <figcaption>Add Your List Here</figcaption>
+                    </figure>
+
+                    <div className="addItems">
+                        <input type="text" placeholder="Add Items..."
+                            value={inputData}
+                            onChange={(e) => setInputData(e.target.value)}
+                        />
+                        {
+                            toggleSubmit ? <button className="submitButton" title="Add Item" onClick={addItem}><b>Add To List</b></button> :
+                                <button className="submitButton" title="Update Item" onClick={addItem}><b>Edit Item</b></button>
+                        }
+
+                    </div>
+
+                    <div className="showItems">
+
+                        {
+                            items.map((elem) => {
+                                return (
+                                    <div className="eachItem" key={elem.id}>
+                                        <h3>{elem.name}</h3>
+                                        <div >
+                                            <button  className="EditButton" title="Edit Item" onClick={() => editItem(elem.id)}>&#128394;</button>
+                                            <button  className="DeleteButton" title="Delete Item" onClick={() => deleteItem(elem.id)}>🗑</button>
+                                        </div>
+                                    </div>
+                                )
+                            })
+
+                        }
+
+                    </div>
+
+                    {/* clear all button  */}
+                    <div className="showItems">
+                        <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll}><span> CHECK LIST </span> </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default TodoApp;
